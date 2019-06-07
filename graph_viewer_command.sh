2ffa343ec12f1f3e6e2a24d5fc066aa2281d4d4a
Himanshu Singh <his795@kepler1.usask.ca>
2019-06-07 13:21:01 -0600

    hey

--- graph_viewer_command.sh
+++ graph_viewer_command.sh
@@ -1,1 +1,2 @@
-graph_viewer cpu 10 1 wg 2.0 1.0 apprcximate "Graph_view/out.ca-AstroPh" "Graph_view/out/ca-AstroPh"
+graph_viewer cpu 1 1 wg 2.0 1.0 apprcximate "Graph_view/ForceAtlasDataset/out.ca-AstroPh copy" "Graph_view/out/ca-AstroPh"
+
