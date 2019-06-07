51fff6e436795457796daba614b1cbc1b2cfc5af
Himanshu Singh <his795@kepler1.usask.ca>
2019-06-07 12:33:33 -0600

    abc1.txt

---
+++ make_then_gdb.sh
@@ -0,0 +1,1 @@
+make graph_viewer && mv graph_viewer ../../../SCoDA_and_GPU/graph_viewer_debug_build_kepler && cd ../../../SCoDA_and_GPU && `cat debug_command.txt` && cd ../GPUGraphLayout/builds/linux
